d1fdf6c9cdca03683cf882f9a7da39e9e9a17e61
양원준 <anyas1398@gmail.com>
2023-08-29 15:27:18 +0900

    Update test2-1.js

--- test2-1.js
+++ test2-1.js
@@ -1,1 +1,8 @@
+function onLoadData() {
+    const result1 = document.createElement('div');
+    result1.textContent = 'add result: ' + add(10, 20);
+    const resultData = document.getElementById('result');
+    resultData.appendChild(result1);
+}
+
 window.onload = onLoadData;
