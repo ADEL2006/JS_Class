f8c6dc3d84720feeec9e11267d9053d00ad809b1
양원준 <anyas1398@gmail.com>
2023-08-29 15:30:38 +0900

    Update test2-1.js

--- test2-1.js
+++ test2-1.js
@@ -1,8 +1,32 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+let sub = function(num1, num2) {
+    return num1 - num2;
+}
+
+let mul = (num1, num2) => {
+    return num1 * num2;
+}
+
+let div = (num1, num2) => {
+    if(num2 == 0) {
+        return 0;
+    }
+    return num1 / num2;
+}
+
 function onLoadData() {
     const result1 = document.createElement('div');
     result1.textContent = 'add result: ' + add(10, 20);
+
+    const result2 = document.createElement('div');
+    result1.textContent = 'add result: ' + add(10, 20);
+
     const resultData = document.getElementById('result');
     resultData.appendChild(result1);
+    resultData.appendChild(result2);
 }
 
 window.onload = onLoadData;
