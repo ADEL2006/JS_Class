b9583b83e2f49a22392ad234e92e14c5146b2e07
양원준 <anyas1398@gmail.com>
2023-08-29 15:07:53 +0900

    .

--- test2.js
+++ test2.js
@@ -10,9 +10,16 @@ let mul = (num1, num2) => {
     return num1 * num2;
 }
 
-let result = add(10, 20);
-console.log('result: ', result);
-result = sub(10, 20);
-console.log('result: ', result);
-result = mul(10, 20);
-console.log('result: ', result);
+function onLoadData(){
+    const result1 = document.createElement('div');
+    result1.textContent = 'add result: ' + add(10,20);
+
+    console.log("html page loaded");
+    let result = add(10, 20);
+    console.log('result: ', result);
+    result = sub(10, 20);
+    console.log('result: ', result);
+    result = mul(10, 20);
+    console.log('result: ', result);
+}
+window.onload = onLoadData();
