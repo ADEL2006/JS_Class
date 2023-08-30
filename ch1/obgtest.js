755e397a9057bb417e900d5da3ef8278963f63b4
양원준 <anyas1398@gmail.com>
2023-08-30 16:34:30 +0900

    Update obgtest.js

--- ch1/obgtest.js
+++ ch1/obgtest.js
@@ -2,11 +2,14 @@ var sayNode = function() {
     console.log("Node");
 }
 
+var num = 1;
+
 var oldObject = {
     sayJs : function() {
         console.log("JS");
     },
     sayNode : sayNode,
 }
+oldObject.sayJs();
 
 console.log(oldObject);
