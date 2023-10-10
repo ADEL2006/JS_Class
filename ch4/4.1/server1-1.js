e003840e3d5c312426cea7f06f8faebd4c9a867a
양원준 <anyas1398@gmail.com>
2023-10-10 15:03:57 +0900

    .

--- ch4/4.1/server1-1.js
+++ ch4/4.1/server1-1.js
@@ -1,5 +1,27 @@
+const res = require('express/lib/response');
 const http = require('http');
 
+http.createServer(async (req, res) => {
+    if (req.method === 'GET') {
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.writable('<h1>Hello Node81! </h1>');
+            res.end('<p>Hello server!</p>');
+        } else if (req.url === '/server2') {
+            try {
+                const data = await fs.readFile('./server2.js');
+                res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+                res.end(data)
+            } catch (err) {
+                console.error(err);
+                res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+                req.end(err.message);
+            }
+        }
+        res.writeHead(404)
+        res.end('404 Not Found');
+    }
+})
 // const server = http.createServer((req, res) => {
 //     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 //     res.write('<h1>Hello Node!</h1>');
