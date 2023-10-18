efd037752c605bfbb5076919b51399bb03054910
양원준 <anyas1398@gmail.com>
2023-10-18 15:08:15 +0900

    .

--- learn-express/app.js
+++ learn-express/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const dotenv = require('dotenv');
 
 const indexRouter = require('./routes');
 const memberRouter = require('./routes/members');
 
+dotenv.config();
 const app = express();
+console.log('process.env.PORT: ' + process.env.PORT);
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
