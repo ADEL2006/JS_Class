ee79a516a7371f27777127f0081d7dd5a5dc5583
양원준 <anyas1398@gmail.com>
2023-10-24 15:27:47 +0900

    .

---
+++ learn-sequelize/models/comment.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+
+class Comment extends Sequelize.Model{
+    static init(sequelize) {
+        return super.init(
+            {
+                comment: {
+                    type: Sequelize.STRING(100),
+                    allowNull: false,
+                },
+                created_at: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.NOW,
+                }
+            },
+            {
+                sequelize,
+                timestemps: false,
+                underscore: false,
+                modelName: 'Comment',
+                tableName: 'comment',
+                paranoid: false,
+                charset: 'utf8',
+                collate:'utf8_unicode_ci',
+            }
+        )
+    }
+    static associate(db) {
+
+    }
+}
+model.exports = Comment;
