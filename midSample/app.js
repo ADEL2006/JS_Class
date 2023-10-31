a1af6ed162fa840837f9efed04c0f88fe4fb2a57
양원준 <anyas1398@gmail.com>
2023-10-31 15:24:49 +0900

    .

--- midSample/app.js
+++ midSample/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const indexRouter = require('./routes/index');
 const studentRouter = require('./routes/student');
+const nunjucks = require('nunjucks');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use('/', indexRouter); // 미들웨어 선언 
 app.use('/student', studentRouter);
+
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
 
 app.listen(app.get('port'), () => {
     console.log('Server Starting port: ', app.get('port'));
--- midSample/routes/student.js
+++ midSample/routes/student.js
@@ -4,12 +4,13 @@ const router = express.Router();
 const students = ['kang', 'kim', 'park', 'moon'];
 
 router.get('/', (req, res) => {
+    console.log('search: ' + typeof(parseInt(req.query.search)) + ', name: ' + req.query.name);
     res.send('Student OK....');
 })
 
 router.get('/:idx', (req, res) => {
-    let index = students[req.params.idx];
-    res.send('Student Param: '+ index);
+    let index = req.params.idx;
+    res.send('Student Param['+ index +'] = ' + students[index]);
 })
 
 module.exports = router;
