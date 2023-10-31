9579dad0bb55a74a6b0211f867a5c15a09686ebd
양원준 <anyas1398@gmail.com>
2023-10-31 15:44:44 +0900

    .

--- midSample/routes/index.js
+++ midSample/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('OK....');
+    // res.send('OK....');
+    res.render('index');
 })
 
 module.exports = router;
