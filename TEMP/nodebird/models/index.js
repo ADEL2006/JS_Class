06d2986af680a996e26a76c3a5c734950a3d0b40
양원준 <anyas1398@gmail.com>
2023-11-14 16:00:02 +0900

    .

--- TEMP/nodebird/models/index.js
+++ TEMP/nodebird/models/index.js
@@ -3,18 +3,13 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
@@ -22,13 +17,14 @@ fs
     return (
       file.indexOf('.') !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-3) === '.js'
     );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file));
+    console.log(file, model.name);
     db[model.name] = model;
+    model.initiate(sequelize);
   });
 
 Object.keys(db).forEach(modelName => {
