91e97bc6c1817a619088a7b12201f80bbd7c4459
양원준 <anyas1398@gmail.com>
2023-11-14 14:49:18 +0900

    Update page.js

--- TEMP/nodebird/routes/page.js
+++ TEMP/nodebird/routes/page.js
@@ -2,4 +2,14 @@ const express = require('express');
 const { renderMain, renderJoin, renderProfile } = require('../controller/page');
 const router = express.Router();
 
+router.use((req, res, next) => {
+    res.locals.user = null;
+    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
+    res.locals.followingList = [];
+})
 router.get('/', renderMain);
+router.get('/join', renderJoin);
+router.get('/profile', renderProfile);
+
+module.exports = router;
