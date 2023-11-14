58cab551d4e9e61000b1d5d0bdebf40062addf9a
양원준 <anyas1398@gmail.com>
2023-11-14 14:10:07 +0900

    Update app.js

--- TEMP/nodebird/app.js
+++ TEMP/nodebird/app.js
@@ -27,5 +27,7 @@ app.use(session({
     }
 }));
 app.use((req, res, next) => {
-    const error = new Error();
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
+    error.status = 404;
+    new(error);
 })
