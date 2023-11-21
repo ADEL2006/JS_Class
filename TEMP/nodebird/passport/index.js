687de28ce5b8723d4c0ad3ada3a053c197b55ad9
양원준 <anyas1398@gmail.com>
2023-11-21 14:33:54 +0900

    .

--- TEMP/nodebird/passport/index.js
+++ TEMP/nodebird/passport/index.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const User = require('../models/user');
+const local = require('./local');
+
 module.exports = () => {
     passport.serializeUser((user, done) => {
         done(null, user.id);
@@ -8,5 +10,6 @@ module.exports = () => {
         User.findOne({where: {id}})
             .then(user => done(null, user))
             .catch(err => done(err));
-    })
+    });
+    local();
 }
