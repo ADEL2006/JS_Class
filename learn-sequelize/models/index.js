172623effc70fff40ac4e6892c7b61af2c1fb741
양원준 <anyas1398@gmail.com>
2023-11-26 23:51:19 +0900

    Update index.js

--- learn-sequelize/models/index.js
+++ learn-sequelize/models/index.js
@@ -6,7 +6,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, 
+const sequelize = new Sequelize(config.database, config.username,
   config.password, config);
 
 db.sequelize = sequelize;
