679380d70bd7904c5ad8e4dd8364a2501f5a2eee
양원준 <anyas1398@gmail.com>
2023-11-26 23:51:43 +0900

    Update index.js

--- learn-sequelize/routes/index.js
+++ learn-sequelize/routes/index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.get('/',async (req,res,next) =>{
+router.get('/', async (req, res, next) => {
     try {
-    const users = await User.findAll();
-    /*const user = await User.create({
-        name: 'kim',
-        age: '53',
-        married: 0,
-      });
-      */
-    res.json(user);
-    } catch(err){
+        const users = await User.findAll();
+        /*const user = await User.create({
+            name: 'kim',
+            age: '53',
+            married: 0,
+          });
+          */
+        res.json(user);
+    } catch (err) {
         console.log(err);
         next(err);
     }
 });
-module.exports=router;
+module.exports = router;
